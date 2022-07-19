3168186196d6d9d163dc9a1ba2c7f076ed231097
Kenneth-Mata <kennethmata1519@gmail.com>
2022-07-19 16:45:44 -0300

    modified:   doom-outrun-electric-theme.el

--- themes/doom-outrun-electric-theme.el
+++ themes/doom-outrun-electric-theme.el
@@ -33,8 +33,8 @@ determine the exact padding."
   "A vibrant, neon colored theme inspired by VS Code Outrun Electric."
 
   ;; name        default   256       16
-  ((bg         '("#221858" "#221858" nil           ))
-   (bg-alt     '("#221858" "#221858" nil           ))
+  ((bg         '("#1b183a" "#1b183a" nil           ))
+   (bg-alt     '("#1b183a" "#1b183a" nil           ))
    (base0      '("#131033" "#131033" "black"       ))
    (base1      '("#1f1147" "#161130" "brightblack" ))
    (base2      '("#110d26" "#110d26" "brightblack" ))
